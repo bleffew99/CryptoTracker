19617f33e7e52f0787174306ccfa670339ffc2d4
bleffew99 <bleffew99@gmail.com>
2017-12-05 22:38:02 -0500

    Initial Commit

---
+++ Final Project2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Final Project2
+//
+//  Created by Brian Leffew on 12/5/17.
+//  Copyright © 2017 Brian Leffew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
